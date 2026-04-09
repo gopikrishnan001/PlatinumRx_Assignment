ea4ab361aa77af7eb41274fbaf94c87267f6edb0
gopikrishnan001 <gokrish2004@gmail.com>
2026-04-09 10:17:46 +0530

    Update 01_Hotel_Schema_Setup.sql

--- SQL/01_Hotel_Schema_Setup.sql
+++ SQL/01_Hotel_Schema_Setup.sql
@@ -24,3 +24,17 @@ CREATE TABLE items (
     item_name VARCHAR(100),
     item_rate FLOAT
 );
+-- Sample data
+INSERT INTO users VALUES
+('21wrcxuy-67erfn', 'John Doe', '97XXXXXXXX', 'john.doe@example.com', 'XX, Street Y, ABC City');
+
+INSERT INTO bookings VALUES
+('bk-09f3e-95hj', '2021-09-23 07:36:48', 'rm-bhf9-aerjn', '21wrcxuy-67erfn');
+
+INSERT INTO items VALUES
+('itm-a9e8-q8fu', 'Tawa Paratha', 18),
+('itm-a07vh-aer8', 'Mix Veg', 89);
+
+INSERT INTO booking_commercials VALUES
+('q34r-3q4o8-q34u', 'bk-09f3e-95hj', 'bl-0a87y-q340', '2021-09-23 12:03:22', 'itm-a9e8-q8fu', 3),
+('q3o4-ahf32-o2u4', 'bk-09f3e-95hj', 'bl-0a87y-q340', '2021-09-23 12:03:22', 'itm-a07vh-aer8', 1);
